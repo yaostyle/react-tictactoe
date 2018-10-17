1681cefd7a3ec03e6ec719d2dfb1848e15ae5e0c
Chris Hsu <yaostyle@gmail.com>
2018-10-17 05:04:44 +0000

    Setup 3x3 boxes board.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
+  
+  constructor(props) {
+    super(props)
+    this.state = {
+      board : Array(9).fill(null)
+    }
+  }
+  
+  handleClick(e) {
+    console.log(e.target)
+  }
+  
   render() {
+    const Box = this.state.board.map(
+      (box, index) => 
+      <div className="box"
+            key={index}
+            onClick={(e) => this.handleClick(e)}>
+      {box}
+      </div>
+)
+    
     return (
-      <h1>Hello React</h1>
+      <div className="container">
+        <h1>Tic Tac Toe App</h1>
+      
+        <div className="board">
+          {Box}
+         
+        </div>
+      </div>
+      
     );
   }
 }
